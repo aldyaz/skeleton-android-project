8817e48151c9d9da2dd946ae3c8a3c32671d2177
Aldyaz Nugroho <aldyaz@Aldyazs-Laptop.local>
2023-05-19 17:12:49 +0700

    Update all dependencies into the latest

--- app/src/main/kotlin/com/example/app/MainApplication.kt
+++ app/src/main/kotlin/com/example/app/MainApplication.kt
@@ -1,12 +1,5 @@
 package com.example.app
 
 import android.app.Application
-import com.facebook.stetho.Stetho
 
-class MainApplication : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-        Stetho.initializeWithDefaults(this)
-    }
-}
+class MainApplication : Application()
